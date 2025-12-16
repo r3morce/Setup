0347f36cb71eaff6d70e00e2d8e3a5a28ee3546f
Mathias Schmidt <mas@interactive-pioneers.de>
2025-12-16 10:17:23 +0100

    feat(fish): add tide

--- stow-packages/fish/.config/fish/config.fish
+++ stow-packages/fish/.config/fish/config.fish
@@ -1,13 +1,15 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-
-    # Color settings for better readability
     set -g fish_color_normal white
     set -g fish_color_param white
     set -g fish_color_option white
 
-    # Aliases
-    alias hx='helix'
+    abbr hx helix
+    abbr n nvim
+    abbr ms make setup
+    abbr gs git status
+    abbr c clear
 end
+
+zoxide init fish | source
 
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
